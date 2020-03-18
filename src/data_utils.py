79b052911d302ef1633f9d085a6dcf164c0c8a30
Sebastian Amenabar <samenabar@uc.cl>
2020-03-18 12:08:11 -0300

    move models code to files more flexible

---
+++ src/data_utils.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def decode_img(img, img_width, img_height):
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_jpeg(img, channels=3)
+    # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # resize the image to the desired size.
+    return tf.image.resize(img, [img_width, img_height])
+
+def process_path(file_path):
+    # label = get_label(file_path)
+    # load the raw data from the file as a string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img
+
+def show_batch(image_batch, labels=None):
+    plt.figure(figsize=(10, 10))
+    for n in range(min(25, image_batch.shape[0])):
+        ax = plt.subplot(5, 5, n + 1)
+        plt.imshow(image_batch[n])
+        try:
+            plt.title(labels[n])
+        except:
+            pass
+        plt.axis('off')
+
+def prepare_for_training(ds, batch_size, cache=True, shuffle_buffer_size=1000):
+    # This is a small dataset, only load it once, and keep it in memory.
+    # use `.cache(filename)` to cache preprocessing work for datasets that don't
+    # fit in memory.
+    if cache:
+        if isinstance(cache, str):
+            ds = ds.cache(cache)
+        else:
+            ds = ds.cache()
+
+    ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+
+    # Repeat forever
+    # ds = ds.repeat()
+
+    ds = ds.batch(batch_size)
+
+    # `prefetch` lets the dataset fetch batches in the background while the model
+    # is training.
+    ds = ds.prefetch(buffer_size=batch_size)
+
+    return ds
---
+++ src/layers.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.constraints import Constraint
+
+POWER_ITERATIONS = 1
+
+
+class AdaIN(Model):
+    def __init__(self, feat_dim, z_dim):
+        super().__init__()
+        self.z_proj = Dense(
+            units=feat_dim * 2,
+            input_shape=(z_dim,),
+            activation='relu',
+            use_bias=True,
+            kernel_initializer=tf.initializers.RandomNormal(stddev=0.02),
+            bias_initializer='zeros',
+        )
+        
+    def call(self, features, z):
+        """
+        Adaptive instance normalization component. Works with both 4D and 5D tensors
+        :features: features to be normalized
+        :scale: scaling factor. This would otherwise be calculated as the sigma from a "style" features in style transfer
+        :bias: bias factor. This would otherwise be calculated as the mean from a "style" features in style transfer
+        """
+        scale, bias = tf.split(self.z_proj(z), 2, num=2, axis=1)
+        mean, variance = tf.nn.moments(features, list(range(len(features.get_shape())))[1:-1],
+                                       keepdims=True)  # Only consider spatial dimension
+        sigma = tf.math.rsqrt(variance + 1e-8)
+        normalized = (features - mean) * sigma
+        scale_broadcast = tf.reshape(scale, tf.shape(mean))
+        bias_broadcast = tf.reshape(bias, tf.shape(mean))
+        normalized = scale_broadcast * normalized
+        normalized += bias_broadcast
+        return normalized
+
+
+def l2_normalize(x, eps=1e-12):
+    '''
+  Scale input by the inverse of it's euclidean norm
+  '''
+    return x / tf.linalg.norm(x + eps)
+
+
+class SpectralNorm(Constraint):
+    '''
+    Uses power iteration method to calculate a fast approximation 
+    of the spectral norm (Golub & Van der Vorst)
+    The weights are then scaled by the inverse of the spectral norm
+    '''
+    def __init__(self, power_iters=POWER_ITERATIONS):
+        self.n_iters = power_iters
+
+    def __call__(self, w):
+        flattened_w = tf.reshape(w, [w.shape[0], -1])
+        u = tf.random.normal([flattened_w.shape[0]])
+        v = tf.random.normal([flattened_w.shape[1]])
+        for i in range(self.n_iters):
+            v = tf.linalg.matvec(tf.transpose(flattened_w), u)
+            v = l2_normalize(v)
+            u = tf.linalg.matvec(flattened_w, v)
+            u = l2_normalize(u)
+        sigma = tf.tensordot(u, tf.linalg.matvec(flattened_w, v), axes=1)
+        return w / sigma
+
+    def get_config(self):
+        return {'n_iters': self.n_iters}
+
+
+class InstanceNorm(Model):
+    def __init__(self, num_channels):
+        super().__init__()
+        self.scale = tf.Variable(initial_value=tf.random.normal((num_channels,), 1.0, 0.02))
+        self.offset = tf.Variable(initial_value=tf.zeros((num_channels,), dtype=tf.float32))
+    
+    def call(self, x):
+        mean, variance = tf.nn.moments(x, axes=[1, 2], keepdims=True)
+        epsilon = 1e-5
+        inv = tf.math.rsqrt(variance + epsilon)
+        normalized = (x - mean) * inv
+        return self.scale * normalized + self.offset
